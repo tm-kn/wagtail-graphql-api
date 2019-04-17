34fcbf3ff5c595d97086ace04ae3306e661713e6
Tomasz Knapik <hi@tmkn.org>
2019-04-17 13:12:28 +0100

    Support lazy types provided to QuerySetList

--- wagtail_graphql/types/structures.py
+++ wagtail_graphql/types/structures.py
@@ -1,3 +1,5 @@
+import inspect
+
 from django.utils.translation import ugettext_lazy as _
 
 from wagtail.search.index import class_is_indexed
@@ -14,18 +16,23 @@ class QuerySetList(graphene.List):
         enable_offset = kwargs.pop('enable_offset', True)
         enable_search = kwargs.pop('enable_search', True)
 
-        if not issubclass(of_type, graphene_django.DjangoObjectType):
+        # Check if the type is a Django model type. Do not perform the
+        # check if value is lazy.
+        if inspect.isclass(of_type) and not issubclass(
+            of_type, graphene_django.DjangoObjectType
+        ):
             raise TypeError(
                 f'{of_type} is not a subclass of DjangoObjectType and it '
                 'cannot be used with QuerySetList.'
             )
-
+        # Enable limiting on the queryset.
         if enable_limit is True and 'limit' not in kwargs:
             kwargs['limit'] = graphene.Argument(
                 PositiveInt,
                 description=_('Limit a number of resulting objects.')
             )
 
+        # Enable offset on the queryset.
         if enable_offset is True and 'offset' not in kwargs:
             kwargs['offset'] = graphene.Argument(
                 PositiveInt,
@@ -35,9 +42,13 @@ class QuerySetList(graphene.List):
                 )
             )
 
+        # If type is provided as a lazy value (e.g. using lambda), then
+        # the search has to be enabled explicitly.
         if (
-            enable_search is True and class_is_indexed(of_type._meta.model)
-            and 'search_query' not in kwargs
+            (enable_search is True and not inspect.isclass(of_type))
+            or (enable_search is True and inspect.isclass(of_type)
+                and class_is_indexed(of_type._meta.model)
+                and 'search_query' not in kwargs)
         ):
             kwargs['search_query'] = graphene.Argument(
                 graphene.String,
