a9620d070d558990a3be05190e92c8f3ce512dc8
Tomasz Knapik <hi@tmkn.org>
2019-04-19 12:00:33 +0100

    Make images query a QuerySetList

--- wagtail_graphql/query_mixins/images.py
+++ wagtail_graphql/query_mixins/images.py
@@ -1,12 +1,14 @@
 from wagtail.images import get_image_model
 
-import graphene
-
 from wagtail_graphql.types import ImageObjectType
+from wagtail_graphql.types import QuerySetList
+from wagtail_graphql.utils import get_base_queryset_for_model_or_qs
 
 
 class ImageQueryMixin:
-    images = graphene.List(ImageObjectType)
+    images = QuerySetList(ImageObjectType)
 
-    def resolve_images(self, info):
-        return get_image_model().objects.all()
+    def resolve_images(self, info, **kwargs):
+        return get_base_queryset_for_model_or_qs(
+            get_image_model().objects.all(), info, **kwargs
+        )
