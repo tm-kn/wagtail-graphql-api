baf34a22b2389c6aa0ee4c270b2902c4af1b134c
Tomasz Knapik <hi@tmkn.org>
2019-04-19 02:02:38 +0100

    Add home page unit test

--- wagtail_graphql/tests/test_pages.py
+++ wagtail_graphql/tests/test_pages.py
@@ -1,4 +1,8 @@
+import json
+
 from django import test
+
+from wagtail.core.models import Site
 
 from graphene_django.views import GraphQLView
 
@@ -6,6 +10,13 @@ from graphene_django.views import GraphQLView
 class TestGraphQLPages(test.TestCase):
     def setUp(self):
         self.request_factory = test.RequestFactory()
+
+    def _graphql_query(self, query):
+        request = self.request_factory.post('/', {
+            'query': query
+        })
+        request.site = Site.objects.first()
+        return GraphQLView.as_view()(request)
 
     def test_graphql_pages_query_returns_200_ok(self):
         query = """
@@ -19,11 +30,9 @@ class TestGraphQLPages(test.TestCase):
             }
         }
         """
-        request = self.request_factory.post('/', {
-            'query': query
-        })
-        response = GraphQLView.as_view()(request)
+        response = self._graphql_query(query)
         self.assertEqual(response.status_code, 200)
+        self.assertNotIn('errors', json.loads(response.content))
 
     def test_graphql_home_page_returns_200_ok(self):
         query = """
@@ -37,8 +46,6 @@ class TestGraphQLPages(test.TestCase):
             }
         }
         """
-        request = self.request_factory.post('/', {
-            'query': query
-        })
-        response = GraphQLView.as_view()(request)
+        response = self._graphql_query(query)
         self.assertEqual(response.status_code, 200)
+        self.assertNotIn('errors', json.loads(response.content))
