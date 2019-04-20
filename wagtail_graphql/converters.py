d1447b7a65cf1f976b67f852c48699e2af06c377
Tomasz Knapik <hi@tmkn.org>
2019-04-20 01:15:53 +0100

    Document converters

--- wagtail_graphql/converters.py
+++ wagtail_graphql/converters.py
@@ -8,14 +8,23 @@ from wagtail_graphql.types.structures import TagList
 
 
 def convert_stream_field(field, _registry=None):
+    """
+    Register a GraphQL scalar for the Wagtail's StreamValue.
+    """
     return StreamField(description=field.help_text, required=not field.null)
 
 
 def convert_tags_to_list_of_strings(field, _registry=None):
+    """
+    Register a GraphQL scalar for the TaggableManager used by Wagtail.
+    """
     return TagList(description=field.help_text, required=not field.null)
 
 
 def register_converters():
+    """
+    Register the custom converters in the graphene-django's registry.
+    """
     convert_django_field.register(WagtailStreamField, convert_stream_field)
     convert_django_field.register(
         TaggableManager, convert_tags_to_list_of_strings
